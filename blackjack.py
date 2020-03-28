f949ea279fe84e936385876495e8aca14de62bbe
Amit Samra <asamra@zipcodes-MacBook-Pro.local>
2020-03-28 17:59:27 -0400

    1 player done, need computer

--- blackjack.py
+++ blackjack.py
@@ -41,40 +41,73 @@ def hit_stay():
 def first_deal():
     pass
 
+def win_check(score):
+    return score == 21
+
+def replay(input):
+    return input("Do you want to play again? Y/N ").lower().startswith('y')
+
 def simple_game(deck):
-    hand = []
-    total = 0
-    #show_deck(deck)
-    acard1 = choose_card(deck)
-    hand.append(acard1)
-    aval1 = return_card_val(acard1)
-    print(f"Card: {acard1} Value: {aval1}")
-    acard2 = choose_card(deck)
-    hand.append(acard2)
-    aval2 = return_card_val(acard2)
-    print(f"Card: {acard2} Value: {aval2}")
-    total = aval1 + aval2
-    print(hand)
-    print(f"Total: {total}")
-    if total == 21:
-        print('You win')
+
+    play_game = input("Are you ready to play? Y/N ")
+    print("-"*50)
+    if play_game.lower() == 'y':
+        game_on = True
     else:
-        while total < 21:
-            hs_choice = hit_stay()
-            if hs_choice == 1:
-                acard3 = choose_card(deck)
-                hand.append(acard3)
-                if acard3 == 'A' and total > 21:
-                    aval3 = 11
-                else:
-                    aval3 = return_card_val(acard3)
-                print(f"Card: {acard3} Value: {aval3}")
-                total += aval3
-                print(hand)
-                print(total)
-            elif hs_choice == 2:
-                break
+        game_on = False
+
+    while game_on:
+
+        hand = []
+        total = 0
+        #show_deck(deck)
+        acard1 = choose_card(deck)
+        hand.append(acard1)
+        aval1 = return_card_val(acard1)
+
+        print("Initial Deal")
+        print("-"*25)
+
+        print(f"Card: {acard1} Value: {aval1}")
+        acard2 = choose_card(deck)
+        hand.append(acard2)
+        aval2 = return_card_val(acard2)
+        print(f"Card: {acard2} Value: {aval2}")
+        total = aval1 + aval2
+        print(f"Current hand {hand}")
+        print(f"Total: {total}")
+        print("-"*25)
+
+        if win_check(total):
+            print('You win!')
+            game_on = False
+
         else:
-            print('You lose')
+            while total < 21:
+                hs_choice = hit_stay()
+                if hs_choice == 1:
+                    acard3 = choose_card(deck)
+                    hand.append(acard3)
+                    if acard3 == 'A' and total > 21:
+                        aval3 = 11
+                    else:
+                        aval3 = return_card_val(acard3)
+                    print(f"Card: {acard3} Value: {aval3}")
+                    total += aval3
+                    print(f"Current hand {hand}")
+                    print(f"Total: {total}")
+                    print("-" * 25)
+                    if win_check(total):
+                        print('You win!')
+                        game_on = False
+                elif hs_choice == 0:
+                    break
+                else:
+                    game_on = False
+            else:
+                print('You lose!')
+                game_on = False
+    else:
+        print('Goodbye')
 
 simple_game(deck1)
