6ce77d43ab4ed0d8daba0b451ad81b9548cccec3
Amit Samra <asamra@zipcodes-MacBook-Pro.local>
2020-03-28 13:13:57 -0400

    Added create deck fcn

--- blackjack.py
+++ blackjack.py
@@ -1,1 +1,23 @@
-a = 'amit'
+from random import randint
+
+rand_num = randint(1,11)
+#print(rand_num)
+
+def create_deck():
+    deck = []
+    for i in ['Diamonds', 'Clubs', 'Hearts', 'Spades']:
+        for j in range(2,11):
+            deck.append( {i:j} )
+            deck.append({'Diamonds': 'J'})
+            deck.append({'Diamonds': 'Q'})
+            deck.append({'Diamonds': 'K'})
+            deck.append({'Diamonds': 'A'})
+    return deck
+
+a = create_deck()
+
+def show_deck(deck):
+    print(deck)
+
+show_deck(a)
+
