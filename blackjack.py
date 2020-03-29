edbbc27bdae40a4822dfea788b908a637cdaf334
Amit Samra <asamra@zipcodes-MacBook-Pro.local>
2020-03-28 22:15:53 -0400

    Added CardGame and Blackjack classes

--- blackjack.py
+++ blackjack.py
@@ -22,23 +22,26 @@ class CardGame():
        shuffle(self.deck)
        print(self.deck)
 
-   def replay(self, input):
+   def replay(self):
        return input("Do you want to play again? Y/N ").lower().startswith('y')
 
 class Blackjack(CardGame):
     def __init__(self, deck_count):
         super().__init__(deck_count)
+        self.values = {
+        2:2, 3:3, 4:4, 5:5, 6:6, 7:7, 8:8, 9:9, 10:10,
+        'J':10, 'Q':10, 'K':10, 'A':11}
 
     def choose_card(self, deck):
         rand_num1 = randint(0, len(self.deck) - 1)
         a = self.deck[rand_num1]
         return a
 
-    def hit_stay():
+    def hit_stay(self):
         hs_op = int(input('Enter 1 to hit or 0 to stay: '))
         return hs_op
 
-    def return_card_val(card):
+    def return_card_val(self,card):
         if type(card) == int:
             card_val = card
         elif card == 'A':
@@ -47,25 +50,25 @@ class Blackjack(CardGame):
             card_val = 10
         return card_val
 
-    def win_check(score):
+    def win_check(self, score):
         return score == 21
 
-    def first_deal():
+    def first_deal(self):
         pass
 
+class Player():
+    def __init__(self, name, score = 0):
+        self.name = name,
+        self.score = score
 
+    def add_score(self):
+        self.score += 1
 
-bj1 = Blackjack(2)
-a = bj1.create_deck()
-print(a)
+bj1 = Blackjack(1)
+deck1 = bj1.create_deck()
 
-""""
-deck1 = create_deck()
-
-
-
-
-
+player1 = Player('Apple')
+print(player1.score)
 
 def simple_game(deck):
 
@@ -81,43 +84,43 @@ def simple_game(deck):
         hand = []
         total = 0
         #show_deck(deck)
-        acard1 = choose_card(deck)
+        acard1 = bj1.choose_card(deck)
         hand.append(acard1)
-        aval1 = return_card_val(acard1)
+        aval1 = bj1.return_card_val(acard1)
 
         print("Initial Deal")
         print("-"*25)
 
         print(f"Card: {acard1} Value: {aval1}")
-        acard2 = choose_card(deck)
+        acard2 = bj1.choose_card(deck)
         hand.append(acard2)
-        aval2 = return_card_val(acard2)
+        aval2 = bj1.return_card_val(acard2)
         print(f"Card: {acard2} Value: {aval2}")
         total = aval1 + aval2
         print(f"Current hand {hand}")
         print(f"Total: {total}")
         print("-"*25)
 
-        if win_check(total):
+        if bj1.win_check(total):
             print('You win!')
             game_on = False
 
         else:
             while total < 21:
-                hs_choice = hit_stay()
+                hs_choice = bj1.hit_stay()
                 if hs_choice == 1:
-                    acard3 = choose_card(deck)
+                    acard3 = bj1.choose_card(deck)
                     hand.append(acard3)
                     if acard3 == 'A' and total > 21:
                         aval3 = 11
                     else:
-                        aval3 = return_card_val(acard3)
+                        aval3 = bj1.return_card_val(acard3)
                     print(f"Card: {acard3} Value: {aval3}")
                     total += aval3
                     print(f"Current hand {hand}")
                     print(f"Total: {total}")
                     print("-" * 25)
-                    if win_check(total):
+                    if bj1.win_check(total):
                         print('You win!')
                         game_on = False
                 elif hs_choice == 0:
@@ -131,4 +134,3 @@ def simple_game(deck):
         print('Goodbye')
 
 simple_game(deck1)
-"""
