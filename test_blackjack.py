ec3385c465e6e3add9ec1d57ac0cbf64d2030c62
Amit Samra <Amit.Samra@outlook.com>
2020-03-31 15:46:29 -0400

    trying to refactor loop

--- test_blackjack.py
+++ test_blackjack.py
@@ -4,21 +4,22 @@ from blackjack import Player
 
 
 class TestCardGame(unittest.TestCase):
-    bj = Blackjack()
+    bj = Blackjack(4)
 
     def test_choose_card(self):
-        self.assertEqual(bj.choose_card(['A',1],'A'),'A')
-        self.assertEqual(bj.choose_card(['A', 1], 1), 1)
+        self.assertEqual(self.bj.choose_card(['A',1]),'A')
+        self.assertEqual(self.bj.choose_card(['A', 1],1), 1)
 
     def test_return_card_val(self):
-        self.assertEqual(bj.return_card_val(['A',1],'A'),11)
-        self.assertEqual(bj.return_card_val(['A',1], 1), 1)
+        self.assertEqual(self.bj.return_card_val('A'),11)
+        self.assertEqual(self.bj.return_card_val(1), 1)
 
 class TestPlayer(unittest.TestCase):
-    pl = Player()
+    pl = Player('Apple')
 
     def test_add_score(self):
-        self.assertEqual(pl.add_score('A'), 11)
+        self.assertEqual(self.pl.add_score(['A']), 11)
 
 if __name__ == '__main__':
     unittest.main()
+
